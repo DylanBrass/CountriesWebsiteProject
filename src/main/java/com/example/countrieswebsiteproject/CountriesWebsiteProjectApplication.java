b9f183a832b69d03737e4aa28639742fc4f0dcc8
DylanBrass <71225455+DylanBrass@users.noreply.github.com>
2022-12-21 15:39:31 -0500

    Created Project

---
+++ src/main/java/com/example/countrieswebsiteproject/CountriesWebsiteProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.countrieswebsiteproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesWebsiteProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountriesWebsiteProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/countrieswebsiteproject/CountriesWebsiteProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.countrieswebsiteproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesWebsiteProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
