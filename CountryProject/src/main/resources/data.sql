8021f1f4f62bc2267982a99a94211ce9c4875a84
DylanBrass <71225455+DylanBrass@users.noreply.github.com>
2022-12-23 00:22:31 -0500

    Update data.sql

--- CountryProject/src/main/resources/data.sql
+++ CountryProject/src/main/resources/data.sql
@@ -26,23 +26,87 @@ VALUES
         ,'Disputed');
 INSERT INTO
     Countries
-(country_name,description,date_founded,flag_past_url,modern_flag_id,map_url )
+(country_name,description,date_founded,flag_past_url,modern_flag_id,map_url,history_text )
 VALUES ('Germany',
         'Germany is a Western European country. It has over 2 millennia of history. Berlin, its capital, is home to art and nightlife scenes, the Brandenburg Gate and many sites relating to WWII.',
          '1949-05-23','https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Flag_of_Germany_%281867%E2%80%931918%29.svg/255px-Flag_of_Germany_%281867%E2%80%931918%29.svg.png'
-         ,2,'germany/germany-regions.json');
+         ,2,'germany/germany-regions.json','Various Germanic tribes have inhabited the northern parts of modern Germany since classical antiquity. A region named Germania was documented before AD 100. In 962, the Kingdom of Germany formed the bulk of the Holy Roman Empire. During the 16th century, northern German regions became the centre of the Protestant Reformation. Following the Napoleonic Wars and the dissolution of the Holy Roman Empire in 1806, the German Confederation was formed in 1815.
+
+Formal unification of Germany into the modern nation-state was commenced on 18 August 1866 with the North German Confederation Treaty establishing the Prussia-led North German Confederation later transformed in 1871 into the German Empire. After World War I and the German Revolution of 1918–1919, the Empire was in turn transformed into the semi-presidential Weimar Republic. The Nazi seizure of power in 1933 led to the establishment of a totalitarian dictatorship, World War II, and the Holocaust. After the end of World War II in Europe and a period of Allied occupation, Germany as a whole was organized into two separate polities with limited sovereignity: the Federal Republic of Germany, generally known as West Germany, and the German Democratic Republic, East Germany, while Berlin continued its Four Power status. The Federal Republic of Germany was a founding member of the European Economic Community and the European Union, while the German Democratic Republic was a communist Eastern Bloc state and member of the Warsaw Pact. After the fall of communism, German reunification saw the former East German states join the Federal Republic of Germany on 3 October 1990—becoming a federal parliamentary republic.
+
+Germany is a great power with a strong economy; it has the largest economy in Europe, the world''s fourth-largest economy by nominal GDP and the fifth-largest by PPP. As a global power in industrial, scientific and technological sectors, it is both the world''s third-largest exporter and importer. As a highly developed country, which ranks ninth on the Human Development Index, it offers social security and a universal health care system, environmental protections, a tuition-free university education, and it is ranked as sixteenth-most peaceful country in the world. Germany is a member of the United Nations, the European Union, NATO, the Council of Europe, the G7, the G20 and the OECD. It has the third-greatest number of UNESCO World Heritage Sites, the most entries in the Memory of the World International Register and the 3rd most Nobel laureates.');
 
 
-INSERT INTO QUESTIONS (ANSWER,FAKE_ANSWER_ONE,FAKE_ANSWER_TWO,FAKE_ANSWER_THREE,QUESTION,COUNTRY_ID) VALUES ('French','German','Joile','English','What language do they speak in France',1);
+INSERT INTO
+    Countries
+(country_name,description,date_founded,flag_past_url,map_url,history_text )
+VALUES ('Ottoman Empire'
+        ,'The Ottoman Empire, also known as the Turkish Empire, was an empire that controlled much of Southeast Europe, Western Asia, and Northern Africa between the 14th and early 20th centuries.'
+        ,'1299-02-02'
+        ,'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Flag_of_the_Ottoman_Empire_%281844%E2%80%931922%29.svg/188px-Flag_of_the_Ottoman_Empire_%281844%E2%80%931922%29.svg.png'
+        ,'turkey/turkiye.json'
+        ,'
+Under the reign of Suleiman the Magnificent, the Ottoman Empire marked the peak of its power and prosperity, as well as the highest development of its governmental, social, and economic systems.[26] At the beginning of the 17th century, the empire contained 32 provinces and numerous vassal states. Some of these were later absorbed into the Ottoman Empire, while others were granted various types of autonomy over the course of centuries.[m] With Constantinople (modern-day Istanbul) as its capital and control of lands around the Mediterranean Basin, the Ottoman Empire was at the centre of interactions between the Middle East and Europe for six centuries.
 
-INSERT INTO QUESTIONS (ANSWER,FAKE_ANSWER_ONE,FAKE_ANSWER_TWO,FAKE_ANSWER_THREE,QUESTION,COUNTRY_ID) VALUES ('Paris','Berlin','Quebec','Brest','What is the capital of France',1);
+While the empire was once thought to have entered a period of decline following the death of Suleiman the Magnificent, this view is no longer supported by the majority of academic historians.[28] The newer academic consensus posits that the empire continued to maintain a flexible and strong economy, society and military throughout the 17th and for much of the 18th century.[29] However, during a long period of peace from 1740 to 1768, the Ottoman military system fell behind that of its European rivals, the Habsburg and Russian empires.[30] The Ottomans consequently suffered severe military defeats in the late 18th and early 19th centuries. The successful Greek War of Independence concluded with decolonization of Greece following the London Protocol (1830) and Treaty of Constantinople (1832). This and other defeats prompted the Ottoman state to initiate a comprehensive process of reform and modernization known as the Tanzimat. Thus, over the course of the 19th century, the Ottoman state became vastly more powerful and organized internally, despite suffering further territorial losses, especially in the Balkans, where a number of new states emerged.[31]
 
-INSERT INTO QUESTIONS (ANSWER,FAKE_ANSWER_ONE,FAKE_ANSWER_TWO,FAKE_ANSWER_THREE,QUESTION,COUNTRY_ID) VALUES ('Rooster','Cow','Bull','Unicorn','What is the national animal of France',1);
+The Committee of Union and Progress (CUP) established the Second Constitutional Era in the Young Turk Revolution in 1908, turning the Empire into a constitutional monarchy, which conducted competitive multi-party elections. However, after the disastrous Balkan Wars, the now radicalized and nationalistic CUP took over the government in the 1913 coup d''état, creating a one-party regime. The CUP allied the Empire with Germany, hoping to escape from the diplomatic isolation which had contributed to its recent territorial losses, and thus joined World War I on the side of the Central Powers.[32] While the Empire was able to largely hold its own during the conflict, it was struggling with internal dissent, especially with the Arab Revolt in its Arabian holdings. During this time, the Ottoman government engaged in genocide against the Armenians, Assyrians, and Greeks.[33] The Empire''s defeat and the occupation of part of its territory by the Allied Powers in the aftermath of World War I resulted in its partitioning and the loss of its Middle Eastern territories, which were divided between the United Kingdom and France. The successful Turkish War of Independence, led by Mustafa Kemal Atatürk against the occupying Allies, led to the emergence of the Republic of Turkey in the Anatolian heartland and the abolition of the Ottoman monarchy.');
 
 
 
-INSERT INTO QUESTIONS (ANSWER,FAKE_ANSWER_ONE,FAKE_ANSWER_TWO,FAKE_ANSWER_THREE,QUESTION,COUNTRY_ID) VALUES ('German','French','Spanish','English','What language do they speak in Germany',2);
+INSERT INTO Modern_Flag
+(DATE_FIRST_USED,DESCRIPTION,FLAG_URL,CREATOR)
+VALUES
+    ('1920-03-30','Two equal horizontal bands of white (top) and red with a blue isosceles triangle based on the hoist side.',
+     'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Flag_of_the_Czech_Republic.svg/383px-Flag_of_the_Czech_Republic.svg.png'
+        ,'Jaroslav Kursa');
 
-INSERT INTO QUESTIONS (ANSWER,FAKE_ANSWER_ONE,FAKE_ANSWER_TWO,FAKE_ANSWER_THREE,QUESTION,COUNTRY_ID) VALUES ('Berlin','Munich','Warsaw','Brest','What is the capital of Germany',2);
 
-INSERT INTO QUESTIONS (ANSWER,FAKE_ANSWER_ONE,FAKE_ANSWER_TWO,FAKE_ANSWER_THREE,QUESTION,COUNTRY_ID) VALUES ('Weimar eagle','Beaver','Horse','German Shepherd','What is the national animal of Germany',2);
+
+INSERT INTO
+    Countries
+(country_name,description,date_founded,flag_past_url,map_url,history_text,modern_flag_id )
+VALUES ('Czech Republic'
+       ,'The Czech Republic, also known as Czechia, is a landlocked country in Central Europe. Historically known as Bohemia, it is bordered by Austria to the south, Germany to the west, Poland to the northeast, and Slovakia to the southeast.'
+       ,'1993-01-01'
+       ,'https://www.tresbohemes.com/wp-content/uploads/2018/04/The-Origin-and-Evolution-of-the-Czech-Flag-Tres-Bohemes-1-.png'
+       ,'czech-republic/czech-republic-regions.json'
+       ,'The Duchy of Bohemia was founded in the late 9th century under Great Moravia. It was formally recognized as an Imperial State of the Holy Roman Empire in 1002 and became a kingdom in 1198.[16][17] Following the Battle of Mohács in 1526, the whole Crown of Bohemia was gradually integrated into the Habsburg monarchy. The Protestant Bohemian Revolt led to the Thirty Years'' War. After the Battle of White Mountain, the Habsburgs consolidated their rule. With the dissolution of the Holy Empire in 1806, the Crown lands became part of the Austrian Empire.
+
+In the 19th century, the Czech lands became more industrialized, and in 1918 most of it became part of the First Czechoslovak Republic following the collapse of Austria-Hungary after World War I.[18] Czechoslovakia was the only country in Central and Eastern Europe to remain a parliamentary democracy during the entirety of the interwar period.[19] After the Munich Agreement in 1938, Nazi Germany systematically took control over the Czech lands. Czechoslovakia was restored in 1945 and became an Eastern Bloc communist state following a coup d''état in 1948. Attempts to liberalize the government and economy were suppressed by a Soviet-led invasion of the country during the Prague Spring in 1968. In November 1989, the Velvet Revolution ended communist rule in the country, and on 1 January 1993, Czechoslovakia was dissolved, with its constituent states becoming the independent states of the Czech Republic and Slovakia.
+
+The Czech Republic is a unitary parliamentary republic and developed country with an advanced, high-income social market economy. It is a welfare state with a European social model, universal health care and free-tuition university education. It ranks 16th in the UN inequality-adjusted human development, 32nd in the Human Development Index and 24th in the World Bank Human Capital Index. It ranks as the 8th safest and most peaceful country and 29th in democratic governance. The Czech Republic is a member of the United Nations, NATO, the European Union, the OECD, the OSCE, and the Council of Europe.'
+,3);
+
+
+
+
+INSERT INTO QUESTIONS (ANSWER,FAKE_ANSWER_ONE,FAKE_ANSWER_TWO,FAKE_ANSWER_THREE,QUESTION,COUNTRY_ID) VALUES ('French','German','Joile','English','What language do they speak in France ?',1);
+
+INSERT INTO QUESTIONS (ANSWER,FAKE_ANSWER_ONE,FAKE_ANSWER_TWO,FAKE_ANSWER_THREE,QUESTION,COUNTRY_ID) VALUES ('Paris','Berlin','Quebec','Brest','What is the capital of France ?',1);
+
+INSERT INTO QUESTIONS (ANSWER,FAKE_ANSWER_ONE,FAKE_ANSWER_TWO,FAKE_ANSWER_THREE,QUESTION,COUNTRY_ID) VALUES ('Rooster','Cow','Bull','Unicorn','What is the national animal of France ?',1);
+
+
+
+INSERT INTO QUESTIONS (ANSWER,FAKE_ANSWER_ONE,FAKE_ANSWER_TWO,FAKE_ANSWER_THREE,QUESTION,COUNTRY_ID) VALUES ('German','French','Spanish','English','What language do they speak in Germany ?',2);
+
+INSERT INTO QUESTIONS (ANSWER,FAKE_ANSWER_ONE,FAKE_ANSWER_TWO,FAKE_ANSWER_THREE,QUESTION,COUNTRY_ID) VALUES ('Berlin','Munich','Warsaw','Brest','What is the capital of Germany ?',2);
+
+INSERT INTO QUESTIONS (ANSWER,FAKE_ANSWER_ONE,FAKE_ANSWER_TWO,FAKE_ANSWER_THREE,QUESTION,COUNTRY_ID) VALUES ('Weimar eagle','Beaver','Horse','German Shepherd','What is the national animal of Germany ?',2);
+
+
+
+
+INSERT INTO QUESTIONS (ANSWER,FAKE_ANSWER_ONE,FAKE_ANSWER_TWO,FAKE_ANSWER_THREE,QUESTION,COUNTRY_ID) VALUES ('Turkish','Greek','Iraqi','English','What language was spoken by the ruler of the Ottoman Empire ?',3);
+
+INSERT INTO QUESTIONS (ANSWER,FAKE_ANSWER_ONE,FAKE_ANSWER_TWO,FAKE_ANSWER_THREE,QUESTION,COUNTRY_ID) VALUES ('Constantinople','Istanbul','Ankara','Athens','What is was the capital of the Ottoman Empire for most of it"s existence ?',3);
+
+INSERT INTO QUESTIONS (ANSWER,FAKE_ANSWER_ONE,FAKE_ANSWER_TWO,FAKE_ANSWER_THREE,QUESTION,COUNTRY_ID) VALUES ('World War 1','Napoleaonic Wars','First Balkan War','World War 2','What war ended the Ottoman Empire ?',3);
+
+
+INSERT INTO QUESTIONS (ANSWER,FAKE_ANSWER_ONE,FAKE_ANSWER_TWO,FAKE_ANSWER_THREE,QUESTION,COUNTRY_ID) VALUES ('Czech','French','polish','German','What language do they speak in Czechia',4);
+
+INSERT INTO QUESTIONS (ANSWER,FAKE_ANSWER_ONE,FAKE_ANSWER_TWO,FAKE_ANSWER_THREE,QUESTION,COUNTRY_ID) VALUES ('Prague','Vienna','Budapest','Brest','What is the capital of Czechia',4);
+
+INSERT INTO QUESTIONS (ANSWER,FAKE_ANSWER_ONE,FAKE_ANSWER_TWO,FAKE_ANSWER_THREE,QUESTION,COUNTRY_ID) VALUES ('the Czech lion','the Czech eagle','the Czech wolf','the Czech bear','What is the national animal of Czechia ?',4);
